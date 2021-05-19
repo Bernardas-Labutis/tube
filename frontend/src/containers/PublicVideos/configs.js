5ff33afeb54525be10da8d76baab4bd39ccf5264
Bernardas Labutis <bernardas.labutis@gmail.com>
2021-05-19 21:00:31 +0300

    more

---
+++ frontend/src/containers/PublicVideos/configs.js
@@ -0,0 +1,62 @@
+import clone from "clone";
+import {
+	DateCell,
+	ImageCell,
+	LinkCell,
+	TextCell,
+} from "../../commonHelpers/helperCells";
+
+const renderCell = (object, type, key) => {
+	const value = object[key];
+	switch (type) {
+		case "ImageCell":
+			return ImageCell(value);
+		case "DateCell":
+			return DateCell(value);
+		case "LinkCell":
+			return LinkCell(value);
+		default:
+			return TextCell(value);
+	}
+};
+
+const columns = [
+	{
+		title: "Title",
+		key: "title",
+		width: 100,
+		render: (object) => renderCell(object, "TextCell", "title"),
+	},
+	{
+		title: "Upload time",
+		key: "uploadTime",
+		width: 100,
+		render: (object) => renderCell(object, "TextCell", "uploadTime"),
+	},
+	{
+		title: "Size",
+		key: "size",
+		width: 200,
+		render: (object) => renderCell(object, "TextCell", "size"),
+	},
+	{
+		title: "Owner",
+		key: "ownerUsername",
+		width: 200,
+		render: (object) => renderCell(object, "TextCell", "ownerUsername"),
+	},
+];
+const editColumns = [
+	{ ...columns[0], width: 300 },
+	{ ...columns[1], width: 300 },
+	columns[2],
+	columns[3],
+];
+const tableinfos = [
+	{
+		title: "Editable View",
+		value: "editView",
+		columns: clone(editColumns),
+	},
+];
+export { columns, tableinfos };
---
+++ frontend/src/containers/PublicVideos/publicvideos.js
@@ -0,0 +1,107 @@
+import React, { Component } from "react";
+import clone from "clone";
+import TableWrapper from "../../commonStyles/table.style";
+import {
+	DeleteCell,
+	EditableCell,
+	DownloadCell,
+} from "../../commonHelpers/helperCells";
+import { tableinfos } from "./configs";
+import axios from "axios";
+import dataMagic from "../../commonHelpers/dataMagic";
+import "react-modal-video/css/modal-video.min.css";
+import ModalVideo from "react-modal-video";
+import PageHeader from "../../components/utility/pageHeader";
+import LayoutWrapper from "../../components/utility/layoutWrapper";
+import ContentHolder from "../../components/utility/contentHolder";
+import Box from "../../components/utility/box";
+import { Row, Col, Button } from "antd";
+import basicStyle from "../../config/basicStyle";
+import Dropzone from "../../components/uielements/dropzone";
+import DropzoneWrapper from "../AdvancedUI/dropzone/dropzone.style";
+import { notification } from "../../components";
+import FormData from "form-data";
+import checkforHeader from "../../axiosheader";
+
+export default class PublicVideos extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			columns: this.createcolumns(clone(tableinfos[0].columns)),
+			dataList: [],
+			isOpen: false,
+			videoUrl: "",
+		};
+		this.openModal = this.openModal.bind(this);
+	}
+	openModal() {
+		this.setState({ isOpen: true });
+	}
+
+	getData() {
+		let data = [];
+		checkforHeader();
+		axios.get("/video/publicAvailable", {}).then((response) => {
+			console.log(response);
+			data = response.data;
+			console.log(data);
+			if (data.length === 0) {
+				this.setState({ dataList: [] });
+			} else {
+				this.setState({
+					dataList: new dataMagic(data.length, data).getAll(),
+				});
+			}
+		});
+	}
+
+	getVideoUrl(videoId) {
+		let videoUrl = "";
+		checkforHeader();
+		axios.get("/video/viewingUrl/" + videoId).then((response) => {
+			console.log(response);
+			this.setState({
+				videoUrl: response.data,
+			});
+		});
+	}
+
+	componentDidMount() {
+		this.getData();
+	}
+
+	createcolumns(columns) {
+		return columns;
+	}
+
+	render() {
+		const { columns, dataList } = this.state;
+		const { rowStyle, colStyle, gutter } = basicStyle;
+		return (
+			<LayoutWrapper>
+				<PageHeader>Public Videos</PageHeader>
+				<Row style={rowStyle} gutter={gutter} justify="start">
+					<Col md={24} sm={24} xs={24} style={colStyle}>
+						<React.Fragment>
+							<TableWrapper
+								columns={columns}
+								dataSource={dataList}
+								onRowClick={(video) => {
+									this.getVideoUrl(video.id);
+									this.openModal();
+								}}
+								className="isoEditableTable"
+							/>
+							<ModalVideo
+								channel="custom"
+								isOpen={this.state.isOpen}
+								url={this.state.videoUrl}
+								onClose={() => this.setState({ isOpen: false })}
+							/>
+						</React.Fragment>
+					</Col>
+				</Row>
+			</LayoutWrapper>
+		);
+	}
+}
